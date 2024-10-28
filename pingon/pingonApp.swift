98c1354ddd8661c61dc4aa8e341168f32237d21d
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-10-28 19:36:55 +0100

    Initial Commit

---
+++ pingon/pingonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pingonApp.swift
+//  pingon
+//
+//  Created by Roland Wenzlofsky on 28.10.24.
+//
+
+import SwiftUI
+
+@main
+struct pingonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pingonTests/pingonTests.swift
@@ -0,0 +1,17 @@
+//
+//  pingonTests.swift
+//  pingonTests
+//
+//  Created by Roland Wenzlofsky on 28.10.24.
+//
+
+import Testing
+@testable import pingon
+
+struct pingonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
